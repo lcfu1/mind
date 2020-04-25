c724000576127ae9e66c86e93ac7e8aaaa04f8d3
lcfu1 <1711648344@qq.com>
2020-04-25 22:05:20 +0800

    修改

--- js/mind.js
+++ js/mind.js
@@ -5,14 +5,16 @@ $(function() {
 		$.getJSON("json/data.json", function (result) {
 			var items=result;
 			
-			if ($("#select").text() == "全部") {
+			if ($("#select").text() == "推荐") {
 				for (var i = 1; i < items.length; i++) {
-					content = content + '<div class="card"><a id="' + items[i].id + '" data-gallery="manual" href="' + items[i].src +
-						'"><img style="min-height: 180px;max-height: 220px;" src="' + items[i].img_thumbnail +
-						'" class="card-img-top" alt="加载出错"></a><div class="card-body"><div class="d-flex"><div class="p-2">' + items[i].title +
-						'</div><div class="p-2 ml-auto"><span class="badge badge-pill badge-danger">' + items[i].category +
-						'</span></div></div><p style="color: #999999;padding-left: .5rem ;">' + items[i].description +
-						'</p></div></div>';
+					if(items[i].recommend>=4){
+						content = content + '<div class="card"><a id="' + items[i].id + '" data-gallery="manual" href="' + items[i].src +
+							'"><img style="min-height: 180px;max-height: 220px;" src="' + items[i].img_thumbnail +
+							'" class="card-img-top" alt="加载出错"></a><div class="card-body"><div class="d-flex"><div class="p-2">' + items[i].title +
+							'</div><div class="p-2 ml-auto"><span class="badge badge-pill badge-danger">' + items[i].category +
+							'</span></div></div><p style="color: #999999;padding-left: .5rem ;">' + items[i].description +
+							'</p></div></div>';
+					}
 				}
 				$(".card-columns").append(content);
 			}
@@ -63,7 +65,7 @@ $(function() {
 				});
 			
 				$(".pdf").click(function() {
-					window.open(items[index].pdfsrc);
+					window.open(items[index].pdfsrc.replace("./", "http://mozilla.github.io/pdf.js/web/viewer.html?file=https://lcfu1.github.io/mind/"));
 				});
 			
 			});
