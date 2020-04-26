aea3f87e83f481f2096d7e0281651515dda5141e
lcfu1 <1711648344@qq.com>
2020-04-26 15:59:52 +0800

    修改

--- js/mind.js
+++ js/mind.js
@@ -1,52 +1,57 @@
 $(function() {
-		var items;
-		var content = '';
-		
 		$.getJSON("json/data.json", function (result) {
 			var items=result;
-			
-			if ($("#select").text() == "推荐") {
-				for (var i = 1; i < items.length; i++) {
-					if(items[i].recommend>=4){
-						content = content + '<div class="card"><a id="' + items[i].id + '" data-gallery="manual" href="' + items[i].src +
-							'"><img style="min-height: 180px;max-height: 220px;" src="' + items[i].img_thumbnail +
-							'" class="card-img-top" alt="加载出错"></a><div class="card-body"><div class="d-flex"><div class="p-2">' + items[i].title +
-							'</div><div class="p-2 ml-auto"><span class="badge badge-pill badge-danger">' + items[i].category +
-							'</span></div></div><p style="color: #999999;padding-left: .5rem ;">' + items[i].description +
-							'</p></div></div>';
-					}
-				}
-				$(".card-columns").append(content);
-			}
+			var content = '';
+			select();
 			
 			$(".dropdown-item").click(function(e) {
 				$(".card").remove();
 				content = '';
 				var category = $(this).text();
 				$("#select").text(category);
-				if (category == "全部") {
+				
+				select();
+			});
+			
+			function select(){
+				var select=$("#select").text();
+				if (select == "推荐") {
+					for (var i = 1; i < items.length; i++) {
+						if(items[i].recommend>=4){
+							content = content + '<div class="card"><a id="' + items[i].id + '" data-gallery="manual" href="' + items[i].src +
+								'"><img style="min-height: 180px;max-height: 220px;" src="' + items[i].img_thumbnail +
+								'" class="card-img-top" alt="加载出错"></a><div class="card-body"><div class="d-flex"><div class="p-2">' + items[i].title +
+								'</div><div class="p-2 ml-auto"><span class="badge badge-pill badge-warning">' + items[i].recommend +
+								'</div><div class="p-2"><span class="badge badge-pill badge-danger">' + items[i].category +
+								'</span></div></div><p style="color: #999999;padding-left: .5rem ;">' + items[i].description +
+								'</p></div></div>';
+						}
+					}
+				}else if (select == "全部") {
 					for (var i = 1; i < items.length; i++) {
 						content = content + '<div class="card"><a id="' + items[i].id + '" data-gallery="manual" href="' + items[i].src +
 							'"><img style="min-height: 180px;max-height: 220px;" src="' + items[i].img_thumbnail +
-							'" class="card-img-top" alt="加载出错"></a><div class="card-body"><div class="d-flex"><div class="p-2">' + items[
-								i].title + '</div><div class="p-2 ml-auto"><span class="badge badge-pill badge-danger">' + items[i].category +
+							'" class="card-img-top" alt="加载出错"></a><div class="card-body"><div class="d-flex"><div class="p-2">' + items[i].title +
+							'</div><div class="p-2 ml-auto"><span class="badge badge-pill badge-warning">' + items[i].recommend +
+							'</div><div class="p-2"><span class="badge badge-pill badge-danger">' + items[i].category +
 							'</span></div></div><p style="color: #999999;padding-left: .5rem ;">' + items[i].description +
 							'</p></div></div>';
 					}
 				} else {
 					for (var i = 1; i < items.length; i++) {
-						if (items[i].category == category) {
+						if (items[i].category == select) {
 							content = content + '<div class="card"><a id="' + items[i].id + '" data-gallery="manual" href="' + items[i].src +
 								'"><img style="min-height: 180px;max-height: 220px;" src="' + items[i].img_thumbnail +
-								'" class="card-img-top" alt="加载出错"></a><div class="card-body"><div class="d-flex"><div class="p-2">' + items[
-									i].title + '</div><div class="p-2 ml-auto"><span class="badge badge-pill badge-danger">' + items[i].category +
+								'" class="card-img-top" alt="加载出错"></a><div class="card-body"><div class="d-flex"><div class="p-2">' + items[i].title +
+								'</div><div class="p-2 ml-auto"><span class="badge badge-pill badge-warning">' + items[i].recommend +
+								'</div><div class="p-2"><span class="badge badge-pill badge-danger">' + items[i].category +
 								'</span></div></div><p style="color: #999999;padding-left: .5rem ;">' + items[i].description +
 								'</p></div></div>';
 						}
 					}
 				}
 				$(".card-columns").append(content);
-			});
+			}
 			
 			$(document).on("click", "[data-gallery=manual]", function(e) {
 				var index = $(this).attr('id');
